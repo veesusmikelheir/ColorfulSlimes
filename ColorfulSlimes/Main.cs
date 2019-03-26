32fa9c4c6d8464f5665b3e336b0905191e024f0b
veesusmikelheir <memebigboy27@hotmail.com>
2019-03-26 11:29:57 -0400

    Initial Commit

---
+++ ColorfulSlimes/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Harmony;
+using MonomiPark.SlimeRancher.Regions;
+using SRML;
+using SRML.SR;
+using SRML.SR.SaveSystem;
+using UnityEngine;
+
+namespace ColorfulSlimes
+{
+    public class Main : ModEntryPoint
+    {
+        const string raveball_key = "t.rave_ball_toy";
+        public override void PreLoad(HarmonyInstance instance)
+        {
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+            
+            TranslationPatcher.AddTranslationKey("pedia",raveball_key,"Rave Ball");
+            var id = IdentifiablePatcher.CreateIdentifiableId(9988, "RAVE_BALL_TOY");
+            var g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject.DontDestroyOnLoad(g);
+            g.AddComponent<RegionMember>();
+            g.AddComponent<Identifiable>().id = id;
+            g.AddComponent<Rigidbody>();
+            g.layer = LayerMask.NameToLayer("Actor");
+            g.AddComponent<Vacuumable>().size = Vacuumable.Size.LARGE;
+            LookupRegistry.RegisterIdentifiablePrefab(g);
+
+
+        }
+        public override void PostLoad()
+        {
+            var toyEntry = new LookupDirector.ToyEntry()
+            {
+                cost = 1000,
+                icon = GameContext.Instance.LookupDirector.toyDict[Identifiable.Id.DISCO_BALL_TOY].icon,
+                nameKey = raveball_key,
+                toyId = (Identifiable.Id) 9988
+            };
+            GameContext.Instance.LookupDirector.toyEntries.Add(toyEntry);
+            GameContext.Instance.LookupDirector.toyDict.Add(toyEntry.toyId, toyEntry);
+
+            foreach (var g in GameContext.Instance.LookupDirector.identifiablePrefabs)
+            {
+                if (Identifiable.IsSlime(Identifiable.GetId(g))&&Identifiable.GetId(g)!=Identifiable.Id.GOLD_SLIME) g.AddComponent<SlimePainter>();
+            }
+        }
+    }
+}
---
+++ ColorfulSlimes/SlimePainter.cs
@@ -0,0 +1,95 @@
+﻿using SRML.SR.SaveSystem;
+using SRML.SR.SaveSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonomiPark.SlimeRancher.Regions;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ColorfulSlimes
+{
+    public class SlimePainter : RegisteredActorBehaviour, ExtendedData.Participant, RegistryFixedUpdateable
+    {
+        private CompoundDataPiece ourPiece;
+
+        private RegionMember regionMember;
+
+        public void Awake()
+        {
+            regionMember = GetComponent<RegionMember>();
+        }
+        public void InitData(CompoundDataPiece piece)
+        {
+            var comp = piece.GetCompoundPiece("slimecolors");
+            comp.SetPiece("top", new Color(Random.value, Random.value, Random.value, Random.value));
+            comp.SetPiece("middle", new Color(Random.value, Random.value, Random.value, Random.value));
+            comp.SetPiece("bottom", new Color(Random.value, Random.value, Random.value, Random.value));
+            comp.SetPiece("isdisco", false);
+        }
+
+        public bool IsDataValid(CompoundDataPiece piece)
+        {
+            Debug.Log("are we valid? "+piece.HasPiece("slimecolors"));
+            return piece.HasPiece("slimecolors");
+        }
+
+        public void SetColors(Color top, Color middle, Color bottom)
+        {
+            foreach (var slimeRenderer in GetComponentsInChildren<Renderer>())
+            {
+                var h = slimeRenderer.material;
+                h.SetColor(topColorNameId, top);
+                h.SetColor(middleColorNameId, middle);
+                h.SetColor(bottomColorNameId, bottom);
+                slimeRenderer.material = h;
+            }
+
+        }
+
+        public void SetColors()
+        {
+            SetColors(
+                ourPiece.GetValue<Color>("top"),
+                ourPiece.GetValue<Color>("middle"),
+                ourPiece.GetValue<Color>("bottom"));
+        }
+
+        
+
+        private static int topColorNameId = Shader.PropertyToID("_TopColor");
+
+        private static int middleColorNameId = Shader.PropertyToID("_MiddleColor");
+
+        private static int bottomColorNameId = Shader.PropertyToID("_BottomColor");
+
+        public void SetData(CompoundDataPiece piece)
+        {
+            
+            ourPiece = piece.GetCompoundPiece("slimecolors");
+
+            SetColors();
+        }
+
+        List<GameObjectActorModelIdentifiableIndex.Entry> toyCache = new List<GameObjectActorModelIdentifiableIndex.Entry>();
+
+        public void CheckRaving()
+        {
+            toyCache.Clear();
+            CellDirector.GetToysNearMember(regionMember, toyCache);
+            ourPiece.SetValue("isdisco",toyCache.Any((x) => x.Id == (Identifiable.Id) 9988));
+        }
+
+        public void RegistryFixedUpdate()
+        {
+            CheckRaving();
+            if (ourPiece.GetValue<bool>("isdisco"))
+            {
+                ourPiece.SetValue("top", new Color(Random.value, Random.value, Random.value, Random.value));
+                ourPiece.SetValue("middle", new Color(Random.value, Random.value, Random.value, Random.value));
+                ourPiece.SetValue("bottom", new Color(Random.value, Random.value, Random.value, Random.value));
+            }
+        }
+    }
+}
